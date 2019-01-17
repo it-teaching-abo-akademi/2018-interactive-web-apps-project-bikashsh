e28bc1e37e62ce1fa552a42a046bb6f3e2074f93
Bikash Shrestha <bikashsh@abo.fi>
2019-01-17 23:52:45 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Portfolio from './Portfolio';
-import { Container } from 'reactstrap';
 
 class App extends Component {
     render() {
@@ -10,9 +9,8 @@ class App extends Component {
                 <div className="appHeader">
                     <h1>Stock Portfolio Management System(SPMS)</h1>
                 </div>
-                <Container fluid>
                 <Portfolio />
-                </Container>
+
             </div>
         );
     }
