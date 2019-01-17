96a60b94c580b84c2373218557de2e7956f97e1d
Bikash Shrestha <bikashsh@abo.fi>
2019-01-17 23:46:39 +0200

    Updates

---
+++ precache-manifest.c387ad4a1eb86025d86762a7d609bd59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "919e30556c7f07b32685",
+    "url": "/2018-interactive-web-apps-project-bikashsh/static/js/runtime~main.919e3055.js"
+  },
+  {
+    "revision": "aa4bc5e2b44936683a3c",
+    "url": "/2018-interactive-web-apps-project-bikashsh/static/js/main.aa4bc5e2.chunk.js"
+  },
+  {
+    "revision": "793e168b48ea10e91523",
+    "url": "/2018-interactive-web-apps-project-bikashsh/static/js/1.793e168b.chunk.js"
+  },
+  {
+    "revision": "aa4bc5e2b44936683a3c",
+    "url": "/2018-interactive-web-apps-project-bikashsh/static/css/main.989b5b7f.chunk.css"
+  },
+  {
+    "revision": "793e168b48ea10e91523",
+    "url": "/2018-interactive-web-apps-project-bikashsh/static/css/1.47a6727d.chunk.css"
+  },
+  {
+    "revision": "24e6549f541874ce21954c4e3c9d5bdc",
+    "url": "/2018-interactive-web-apps-project-bikashsh/index.html"
+  }
+];
